064459f65ddc6354785bbd780b7c0df3052d85ff
kylelbs <kyle.lebrissaget@gmail.com>
2022-08-01 23:21:18 +0200

    added divs within div lines (grid creation)

--- script.js
+++ script.js
@@ -7,4 +7,8 @@ for (let i = 0; i < 16; i++) {
     divline = document.createElement("div");
     divline.classList.add(`line${i}`);
     grid.appendChild(divline);
+    for (let i = 0; i < 16; i++) {
+        subdiv = document.createElement("div");
+        divline.appendChild(subdiv);
+    };
 };
