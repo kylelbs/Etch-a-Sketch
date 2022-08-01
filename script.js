1e142021624cbac0a706460fff09c0488e1e7d51
kylelbs <kyle.lebrissaget@gmail.com>
2022-08-02 00:43:32 +0200

    added color hover on pixels

--- script.js
+++ script.js
@@ -1,15 +1,22 @@
 console.log("Hello");
-grid = document.createElement("div");
+let grid = document.createElement("div");
 grid.classList.add("grid");
 document.body.appendChild(grid);
 
 for (let i = 0; i < 16; i++) {
-    divline = document.createElement("div");
+    let divline = document.createElement("div");
     divline.classList.add('divline');
     grid.appendChild(divline);
     for (let i = 0; i < 16; i++) {
-        divpixel = document.createElement("div");
+        let divpixel = document.createElement("div");
         divpixel.classList.add('divpixel')
         divline.appendChild(divpixel);
     };
 };
+
+let divpixels = document.querySelectorAll('.divpixel');
+divpixels.forEach(divpixel => {
+    divpixel.addEventListener('mouseover', function (e) {
+        e.target.style.backgroundColor = "red";
+    })
+})
