59884a2504b2ba14be14c586528c104312e18530
kylelbs <kyle.lebrissaget@gmail.com>
2022-08-03 00:10:39 +0200

    Gridsize button is now working

--- script.js
+++ script.js
@@ -3,20 +3,63 @@ let grid = document.createElement("div");
 grid.classList.add("grid");
 document.body.appendChild(grid);
 
-for (let i = 0; i < 16; i++) {
-    let divline = document.createElement("div");
-    divline.classList.add('divline');
-    grid.appendChild(divline);
-    for (let i = 0; i < 16; i++) {
-        let divpixel = document.createElement("div");
-        divpixel.classList.add('divpixel')
-        divline.appendChild(divpixel);
+let divline;
+let divwrapper;
+
+function createGrid(squrperside) {
+    for (let i = 0; i < squrperside; i++) {
+        divline = document.createElement("div");
+        divline.classList.add('divline');
+
+        divwrapper = document.createElement("div");
+        divwrapper.classList.add('divwrapper');
+        divline.style.display = "flex";
+        // divline.style.flexDirection = "row";
+        divwrapper.style.display = "flex";
+        grid.appendChild(divline);
+        divline.appendChild(divwrapper);
+        for (let i = 0; i < squrperside; i++) {
+            let divpixel = document.createElement("div");
+            divpixel.classList.add('divpixel')
+            divwrapper.appendChild(divpixel);
+        };
     };
+    console.log(divwrapper.offsetWidth);
 };
+
+createGrid(16);
+
+
+
+// for (let i = 0; i < 16; i++) {
+//     let divline = document.createElement("div");
+//     divline.classList.add('divline');
+//     grid.appendChild(divline);
+//     for (let i = 0; i < 16; i++) {
+//         let divpixel = document.createElement("div");
+//         divpixel.classList.add('divpixel')
+//         divline.appendChild(divpixel);
+//     };
+// };
 
 let divpixels = document.querySelectorAll('.divpixel');
 divpixels.forEach(divpixel => {
     divpixel.addEventListener('mouseover', function (e) {
         e.target.style.backgroundColor = "red";
     });
+});
+
+const gridsizebutton = document.querySelector("#gridsize");
+gridsizebutton.addEventListener('click', function () {
+    userSize = prompt("Please enter a number of squares per side for the new grid:");
+    // console.log(userSize);
+    grid.innerHTML = "";
+    createGrid(userSize);
+    let divpixels = document.querySelectorAll('.divpixel');
+    divpixels.forEach(divpixel => {
+        divpixel.addEventListener('mouseover', function (e) {
+            e.target.style.backgroundColor = "red";
+        });
+    });
+
 });
